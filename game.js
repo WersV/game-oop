025716860ad7b9de6620b3a713f71d82f15668c8
Przemysław Botwina <przemek.botwina@o2.pl>
2022-10-07 21:38:24 +0200

    add game.js with imported Wallet and Stat.. class

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
 // link do gry: https://websamuraj.pl/examples/js/gra/ 
+
+import Wallet from './Wallet.js';
+import Statistics from './Statistics.js';
+
+const wallet = new Wallet(100);
+const statistics = new Statistics();
+
+wallet.changeWallet(10, '+');
+wallet.getWalletValue();
+
+statistics.showGameStatistics();
+// console.log(statistics.gameResults);
