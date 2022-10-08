a34f620cccacba1f99e5679dbe13fe5bcf9d051f
Przemysław Botwina <przemek.botwina@o2.pl>
2022-10-08 08:20:55 +0200

    add Draw.js and import it to Game.js

--- Draw.js
+++ Draw.js
@@ -1,3 +1,16 @@
 export default class Draw {
-  constructor() {}
+  constructor() {
+    this.options = ['red', 'green', 'blue'];
+    let _result = this.drawResult();
+    this.getDrawResult = () => console.log(_result);
+  }
+
+  drawResult() {
+    let colors = [];
+    for (let i = 0; i < this.options.length; i++) {
+      const index = Math.floor(Math.random() * this.options.length)
+      colors.push(this.options[index]);
+    }
+    return colors
+  }
 }
--- game.js
+++ game.js
@@ -2,12 +2,14 @@
 
 import Wallet from './Wallet.js';
 import Statistics from './Statistics.js';
+import Draw from './Draw.js';
 
 const wallet = new Wallet(100);
 const statistics = new Statistics();
+const draw = new Draw();
 
 wallet.changeWallet(10, '+');
-wallet.getWalletValue();
 
 statistics.showGameStatistics();
-// console.log(statistics.gameResults);
+
+draw.getDrawResult();
